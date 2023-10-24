edcd309d5441d6fc0a96c01b89269e8c70c96432
Pavel Kravtsov <kravtsovpo@gmail.com>
2023-10-24 14:39:59 +0800

    Add ClickHouseMigrateAsync example

--- src/ClickHouse.Facades.Example/Program.cs
+++ src/ClickHouse.Facades.Example/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using ClickHouse.Facades;
+using ClickHouse.Facades.Example;
+using ClickHouse.Facades.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = CreateHostBuilder(args).Build();
+
+var serviceProvider = host.Services;
+
+await serviceProvider.ClickHouseMigrateAsync();
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+	Host.CreateDefaultBuilder(args)
+		.ConfigureAppConfiguration((context, builder) =>
+		{
+			var path = Path.Combine(context.HostingEnvironment.ContentRootPath, "appsettings.json");
+			builder.AddJsonFile(path, false, true);
+		})
+		.ConfigureServices((_, services) =>
+		{
+			services.AddOptions<ClickHouseConfig>()
+				.BindConfiguration(nameof(ClickHouseConfig));
+
+			services.AddClickHouseMigrations<ClickHouseMigrationInstructions, ClickHouseMigrationsLocator>();
+		});
