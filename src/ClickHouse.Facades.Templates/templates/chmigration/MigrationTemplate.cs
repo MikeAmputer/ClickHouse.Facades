66aea7209abbf3a46837930c9a9a2c468c5f0687
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-05-24 01:21:44 +0700

    Add migration template

---
+++ src/ClickHouse.Facades.Templates/templates/chmigration/MigrationTemplate.cs
@@ -0,0 +1,19 @@
+﻿using ClickHouse.Facades.Migrations;
+
+// ReSharper disable InconsistentNaming
+
+namespace $namespace$;
+
+[ClickHouseMigration($index$, "$title$")]
+public class $title$_ClickHouseMigration : ClickHouseMigration
+{
+	protected override void Up(ClickHouseMigrationBuilder migrationBuilder)
+	{
+		// migrationBuilder.AddRawSqlStatement("create table if not exists ...")
+	}
+
+	protected override void Down(ClickHouseMigrationBuilder migrationBuilder)
+	{
+		// migrationBuilder.AddRawSqlStatement("drop table if exists ...")
+	}
+}
