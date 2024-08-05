c073ffe0fb63f9e283c5bfe624fed5d56cd46b28
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-08-05 22:54:37 +0700

    Add query logging through IClickHouseCommandExecutionListener to Example.Simple

---
+++ examples/Example.Simple/Logging/QueryLogger.cs
@@ -0,0 +1,28 @@
+﻿using ClickHouse.Client.ADO;
+using Microsoft.Extensions.Logging;
+
+namespace ClickHouse.Facades.Example;
+
+public class QueryLogger : IClickHouseCommandExecutionListener
+{
+	private readonly ILogger _logger;
+
+	public QueryLogger(ILogger<QueryLogger> logger)
+	{
+		ArgumentNullException.ThrowIfNull(logger);
+
+		_logger = logger;
+	}
+
+	public async Task ProcessExecutedCommand(ClickHouseCommand command, CancellationToken cancellationToken = default)
+	{
+		await Task.Yield();
+
+		var stats = command.QueryStats;
+
+		if (stats != null && stats.ReadRows > 0)
+		{
+			_logger.LogDebug($"{command.CommandText}\nReadRows: {stats.ReadRows} | ReadBytes: {stats.ReadBytes}");
+		}
+	}
+}
