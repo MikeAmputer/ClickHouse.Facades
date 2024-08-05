2dda404734a99771b64c3c902c9adcdc0ddc1429
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-08-05 22:53:37 +0700

    Add command execution listener

---
+++ src/ClickHouse.Facades/Context/Command/IClickHouseCommandExecutionListener.cs
@@ -0,0 +1,8 @@
+﻿using ClickHouse.Client.ADO;
+
+namespace ClickHouse.Facades;
+
+public interface IClickHouseCommandExecutionListener
+{
+	public Task ProcessExecutedCommand(ClickHouseCommand command, CancellationToken cancellationToken = default);
+}
---
+++ src/ClickHouse.Facades/Context/Connection/ConnectionBrokerParameters.cs
@@ -0,0 +1,12 @@
+﻿using ClickHouse.Client.ADO;
+
+namespace ClickHouse.Facades;
+
+internal class ConnectionBrokerParameters
+{
+	public ClickHouseConnection? Connection { get; set; }
+
+	public ICommandExecutionStrategy? CommandExecutionStrategy { get; set; }
+
+	public IClickHouseCommandExecutionListener? CommandExecutionListener { get; set; }
+}
