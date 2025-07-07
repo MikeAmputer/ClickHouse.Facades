67a08ba71c410649a9272ce0da6090fb51e242d5
Pavel Kravtsov <kravtsovpo@gmail.com>
2025-07-07 19:02:46 +0700

    Add abstract aggregate migrations locator

---
+++ src/ClickHouse.Facades/Migrations/Instructions/AggregateClickHouseMigrationsLocator.cs
@@ -0,0 +1,11 @@
+﻿namespace ClickHouse.Facades.Migrations;
+
+public abstract class AggregateClickHouseMigrationsLocator : IClickHouseMigrationsLocator
+{
+	protected abstract IEnumerable<IClickHouseMigrationsLocator> Locators { get; }
+
+	public IEnumerable<ClickHouseMigration> GetMigrations()
+	{
+		return Locators.SelectMany(locator => locator.GetMigrations());
+	}
+}
