29e681d420710987bdb1e257a2a402184e101423
Pavel Kravtsov <kravtsovpo@gmail.com>
2026-03-27 14:22:00 +0700

    Use dotnet format whitespace

--- examples/Example.Simple/Context/UserExpenses.cs
+++ examples/Example.Simple/Context/UserExpenses.cs
@@ -2,7 +2,7 @@
 
 namespace ClickHouse.Facades.Example;
 
-public record UserExpenses(uint UserId,  decimal Expenses)
+public record UserExpenses(uint UserId, decimal Expenses)
 {
 	public static UserExpenses FromReader(DbDataReader reader) =>
 		new(reader.GetFieldValue<uint>(0), reader.GetDecimal(1));
